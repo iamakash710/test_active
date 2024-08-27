70dcfc38b0fe8a319583a06fa48daf1a0a97b342
Akash <iamakash.karmakar710@gmail.com>
2024-08-27 23:06:17 +0530

    added cat feature

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from src.properties.const import const    
 
 
-def test():
-    user_input = input()
+def test(user_input):
+    
     if user_input == "dog":
         print ("woof")
+    elif user_input == "cat":
+        print("meow")
     else:
         print ("not valid")
 
 if __name__ == "__main__":
-    a = test()
+    user_input = input("enter text here:")
+    a = test(user_input=user_input)
     print(a)
     print("executed")
     
