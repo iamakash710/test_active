18c0a9c315c12cf477f0040e93a8b8119d895bc3
iamakash710 <iamakash.karmakar710@gmail.com>
2024-08-27 22:06:00 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.properties.const import const    
+
+
+def test():
+    if const == "abc":
+        print (True)
+    else:
+        print (False)
+
+if __name__ == "__main__":
+    a = test()
+    print(a)
+    print("executed")
+    
